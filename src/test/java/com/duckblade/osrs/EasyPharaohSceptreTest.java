c22d35948bc5c106655ee3cdcd26343b3c8b97b1
Paul Norton <napkinorton@gmail.com>
2022-04-30 01:06:34 -0700

    Create plugin

--- src/test/java/com/duckblade/osrs/EasyPharaohSceptreTest.java
+++ src/test/java/com/duckblade/osrs/EasyPharaohSceptreTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.duckblade.osrs;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class EasyPharaohSceptreTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(EasyPharaohSceptre.class);
 		RuneLite.main(args);
 	}
 }
